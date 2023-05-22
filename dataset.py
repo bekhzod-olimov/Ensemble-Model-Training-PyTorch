3bb978650ca6295960a72db6ef6d3954efcdacd7
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-05-22 13:21:43 +0900

    dataset script

--- dataset.py
+++ dataset.py
@@ -16,10 +16,13 @@ class CustomDataset(Dataset):
     
     def __init__(self, data_path):
         
+        # Read csv data
         df = pd.read_csv(data_path)
-        self.ims = df.iloc[:,1:].values.reshape(df.shape[0], 28, 28)
-        self.lbls = df.iloc[:,0].values.reshape(df.shape[0], 1)
+        # Get images and their corresponding labels
+        self.ims, self.lbls = df.iloc[:,1:].values.reshape(df.shape[0], 28, 28), df.iloc[:,0].values.reshape(df.shape[0], 1)
     
+    # Get dataset length
     def __len__(self): return len(self.ims)
     
+    # Get data samples based on index
     def __getitem__(self, idx): return torch.FloatTensor(self.ims[idx]).unsqueeze(0), torch.LongTensor(self.lbls[idx]).squeeze()
